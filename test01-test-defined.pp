a13aea8e2d52c4a590d5c97fa604679c42a10dc6
James Zhu <zhujian0805@gmail.com>
2016-03-17 19:53:00 +0800

    D  modules/motd/Modulefile D  modules/motd/README D  modules/motd/manifests/init.pp D  modules/motd/spec/spec_helper.rb D  modules/motd/tests/init.pp D  test-concat.pp  M test01-test-defined.pp

--- test01-test-defined.pp
+++ test01-test-defined.pp
@@ -1,1 +1,8 @@
-include test01
+define tttt( $something = "Testing define" ) {
+    notice ("This is the testing for self-define resource!!!")
+    notice ( $something )
+}
+
+tttt { 'testing':
+    something => "This is the testing string for testing define",
+}
