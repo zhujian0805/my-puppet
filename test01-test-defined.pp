c3e455b634536c7bde4219797214ab21702b6b5d
James Zhu <zhujian0805@gmail.com>
2016-03-17 20:01:02 +0800

    D  test01-test-defined.pp

--- test01-test-defined.pp
+++
@@ -1,8 +0,0 @@
-define tttt( $something = "Testing define" ) {
-    notice ("This is the testing for self-define resource!!!")
-    notice ( $something )
-}
-
-tttt { 'testing':
-    something => "This is the testing string for testing define",
-}
