d7c767f0510e41b44f8922e2d7b637f25c2cbf56
James Zhu <zhujian0805@gmail.com>
2016-03-17 12:38:25 +0800

    M modules/test01/manifests/init.pp  M testing-scripts.sh

--- testing-scripts.sh
+++ testing-scripts.sh
@@ -2,6 +2,9 @@
 
 # Testign the test01-mod01.pp which doesn't has init.pp
 puppet apply --modulepath modules test01-mod01.pp
-
+echo
+echo
 # Testing the 'defined' function
 puppet apply --modulepath modules test01-test-defined.pp
+echo
+echo
