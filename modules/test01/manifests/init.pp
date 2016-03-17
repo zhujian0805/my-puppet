5ee2173c232b9bbc0991828998960138b90d713f
James Zhu <zhujian0805@gmail.com>
2016-03-17 12:35:41 +0800

    M testing-scripts.sh ?? modules/test01/manifests/init.pp ?? test01-test-defined.pp

---
+++ modules/test01/manifests/init.pp
@@ -0,0 +1,9 @@
+class test01 {
+	    if defined( "test01::fucking") {
+	            notify {'oh yes, test01::fucking is defined':}
+	    }
+}
+
+class test01::fucking {
+
+}
--- testing-scripts.sh
+++ testing-scripts.sh
@@ -2,3 +2,6 @@
 
 # Testign the test01-mod01.pp which doesn't has init.pp
 puppet apply --modulepath modules test01-mod01.pp
+
+# Testing the 'defined' function
+puppet apply --modulepath modules test01-test-defined.pp
