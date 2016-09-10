29d2a9db807b5f9d74988432b454cf17555f0387
James Zhu <zhujian0805@gmail.com>
2016-09-10 21:54:53 +0800

    D test-defined-type-in-array.pp  D test-if-class-defined.pp  D test01-mod01.pp  D test01-test-defined-function.pp  D test01-test-if-defined.pp ?? samples/

--- test-defined-type-in-array.pp
+++
@@ -1,8 +0,0 @@
-define tttt( $something = "Testing define" ) {
-    notice ("This is the testing for self-define resource!!!")
-    notice ( $name )
-}
-
-$more = [ 'one', 'two', 'three' ]
-
-tttt { $more: }
--- test01-mod01.pp
+++
@@ -1,1 +0,0 @@
-include test01::mymod
