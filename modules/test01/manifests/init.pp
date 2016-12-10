7f9b416ff9b2a42402076299ad68448285def4d6
James Zhu <zhujian0805@gmail.com>
2016-12-10 17:57:10 +0800

    M init.pp  M mymod.pp

--- modules/test01/manifests/init.pp
+++ modules/test01/manifests/init.pp
@@ -1,10 +1,12 @@
+# Docs
 class test01 {
-	    if defined( "test01::fucking") {
-	            notify {'oh yes, test01::fucking is defined':}
-                include test01::fucking
-	    }
+    if defined( 'test01::fucking') {
+        notify {'oh yes, test01::fucking is defined':}
+        include test01::fucking
+    }
 }
 
+# Docs
 class test01::fucking {
     notify {'oh yes, its test01::fucking included!!':}
 }
--- modules/test01/manifests/mymod.pp
+++ modules/test01/manifests/mymod.pp
@@ -1,3 +1,4 @@
+# This is a test class
 class test01::mymod {
   notice ('hello world!!!')
 }
