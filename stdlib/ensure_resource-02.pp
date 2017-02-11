a49d06b789230745ade0b4e20abfe1781be27de7
James Zhu <zhujian0805@gmail.com>
2017-02-11 20:07:44 +0800

    This commit has the following change(s): ------------------------------------------------  M stdlib/ensure_resource-02.pp ------------------------------------------------

--- stdlib/ensure_resource-02.pp
+++ stdlib/ensure_resource-02.pp
@@ -2,7 +2,7 @@ class cuser {
     user { 'dan':
       ensure => present,
     }
-    ensure_resource('user', 'dan', {'ensure' => 'present' , uid => '766' })
+    ensure_resource('user', 'james', {'ensure' => 'present' , uid => '766' })
 }
 
 #include stdlib
