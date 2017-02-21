69f9fd84fa5453a6b2aab4679ad6f2d705e0b2c8
James Zhu <zhujian0805@gmail.com>
2017-02-21 21:03:14 +0800

    This commit has the following change(s): ------------------------------------------------  M modules/test01/manifests/mymod.pp ------------------------------------------------

--- modules/test01/manifests/mymod.pp
+++ modules/test01/manifests/mymod.pp
@@ -1,4 +1,6 @@
 # This is a test class
+#See the difference with/without inherits
+#class test01::mymod inherits test01 { 
 class test01::mymod {
   notify { "This is test01::mymod": }
   notice ('hello world!!!')
