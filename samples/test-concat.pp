dbd1adb0e00fc7f9c5c41f908e823ef8378d8244
James Zhu <zhujian0805@gmail.com>
2017-03-10 10:48:22 +0800

    This commit has the following change(s): ------------------------------------------------  M samples/test-concat.pp ------------------------------------------------

--- samples/test-concat.pp
+++ samples/test-concat.pp
@@ -1,8 +1,10 @@
-concat { '/tmp/file':
-  ensure => present,
-}
-
-concat::fragment { 'tmpfile':
-  target  => '/tmp/file'
-  content => 'test contents',
+class cct {
+	concat { '/tmp/file':
+	  ensure => present,
+	}
+	
+	concat::fragment { 'tmpfile':
+	  target  => '/tmp/file'
+	  content => 'test contents',
+	}
 }
